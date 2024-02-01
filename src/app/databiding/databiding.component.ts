5fac0073c2e0dd01f575bedaad1fd1b0710c78f4
Bruna Garcia <47999868+GarciaBoo@users.noreply.github.com>
2024-02-01 10:26:04 -0300

    Directives Practice
    Final update of section 2 from course.

--- src/app/databiding/databiding.component.ts
+++ src/app/databiding/databiding.component.ts
@@ -12,6 +12,7 @@ export class DatabidingComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverCreated = false;
+  servers = ['Testeserver', 'Testserver 2'];
 
   constructor() {
   setTimeout(() => {
@@ -24,6 +25,7 @@ export class DatabidingComponent implements OnInit{
   onCreateServer(){
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: any){
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,23 @@ import { Component } from "@angular/core"; //Importa o decorador component
 @Component({ //Define o que é o componente usando um objeto js
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrl: './server.component.css'
+    styleUrl: './server.component.css',
+    styles: [`.online{ color: white;}`]
+
 })
 export class ServerComponent {
-    
+    ServerID: number = 11;
+    serverStatus: string = 'offiline';
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : "offiline";
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? "green" : "red";
+    }
 }
